333a8eaa6f3316d0e159df7bbbf84fd76d8a4278
silent-one <daniel.hampton@gmail.com>
2014-08-27 13:52:52 +1200

    Add loading screen tada

--- js/app.js
+++ js/app.js
@@ -2,6 +2,27 @@
 
 	var app = angular.module('splatter-web', ['ngResource']);
 
+	app.config(function($provide) {
+		$provide.decorator('$q', ['$delegate', '$rootScope', function($delegate, $rootScope) {
+		  var pendingPromisses = 0;
+		  $rootScope.$watch(
+			function() { return pendingPromisses > 0; }, 
+			function(loading) { $rootScope.loading = loading; }
+		  );
+		  var $q = $delegate;
+		  var origDefer = $q.defer;
+		  $q.defer = function() {
+			var defer = origDefer();
+			pendingPromisses++;
+			defer.promise.finally(function() {
+			  pendingPromisses--;
+			});
+			return defer;
+		  };
+		  return $q;
+		}]);
+	});
+
 	app.factory("User", function($resource){
 		return $resource("http://hampton.sqrawler.com:3000/users/:id");
 	});
